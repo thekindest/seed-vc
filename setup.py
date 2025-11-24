6adb4e2a8aad1ecf6db0707d52a8c1a4501e629a
sam <azsam204@gmail.com>
2025-11-24 01:22:21 +0000

    package py_modules seed_vc_wrapper and helpers

--- setup.py
+++ setup.py
@@ -4,4 +4,10 @@ setup(
     name="seed-vc",
     version="0.0.0",
     packages=find_packages(),
+    py_modules=[
+        "seed_vc_wrapper",
+        "hf_utils",
+        "optimizers",
+    ],
+    include_package_data=True,
 )
